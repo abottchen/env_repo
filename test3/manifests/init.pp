3b3c4f5edf6e3cc5f019116462a968e34c8b2b20
Adam Bottchen <adam.bottchen@puppet.com>
2016-12-24 09:25:14 -0800

    changes

--- test3/manifests/init.pp
+++ test3/manifests/init.pp
@@ -1,3 +1,10 @@
 class test3 {
-  notify {'test3':}
+  $ts = generate('/bin/date', '+%Y%d%m_%H:%M:%S')
+  $timestamp = regsubst($ts,'\n','')
+
+  file {'/tmp/testfile':
+    ensure  => file,
+    content => 'Testing content',
+    backup  => ".${timestamp}",
+  }
 }
